77afaa5aa3c962bd2414eadffb82a16ebb3a5cad
Danny Joseph <DjosephNP@gmail.com>
2023-04-10 22:29:03 -0400

    Changed port number to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 
 // Setup a port to Listen on.
 // If a port isn't specified, then use port 8080 as a fallback
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 8080 
 
 console.log("Listening to Port: " + port)
 app.listen(port);

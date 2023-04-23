8bddb8693daeaa1b5cc6bf20bbde1ffadf589c9a
Nicholas Haug <njhaug6815@eagle.fgcu.edu>
2023-04-23 16:04:58 -0400

    dashboard sorts
    dashboard now sorta listings

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,14 +8,22 @@ router.use(json());
 
 router.get('/',  async (req, res) => {
   try {
+    const sortBy = req.query.sortBy || 'company_name'; // Set a default sorting column
+
+    // prevent SQL injection
+    const allowedSortColumns = ['company_name', 'location', 'industry', 'start_date', 'semester', 'tag_description'];
+    if (!allowedSortColumns.includes(sortBy)) {
+      throw new Error('Invalid sort column');
+    }
     const internships = await db.any(`
       SELECT i.job_id, c.name as company_name, i.location, c.industry, i.start_date, i.semester, t.description as tag_description
       FROM internship i
       JOIN company c ON i.company_id = c.company_id
       JOIN tag t ON i.tag_id = t.tag_id
+      ORDER BY ${sortBy}
     `);
 
-    res.render('dashboard', { internships });
+    res.render('dashboard', { internships, sortBy });
   } catch (error) {
     console.error('Error fetching internships:', error.message);
     console.error('Error details: ', error)
